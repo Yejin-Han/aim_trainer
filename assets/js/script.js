27f20a63c00e1f8c0f9eeb9393730446d1a0bd4f
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-04-24 17:32:21 +0900

    페북 공유 테스트
    ... 실제로 내가 해볼순 없지만 일단,,
    
    + url을 벡틱으로 바꿨다

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,18 +157,28 @@ const copyToClipBoard = async () => {
   }
 }
 
+const shareKakao = () => {
+
+}
+
 const shareNaver = () => {
   const title = encodeURIComponent("나의 에임 점수는?");
   const url = encodeURIComponent(window.location.href);
-  window.open("https://share.naver.com/web/shareView?url=" + url + "&title=" + title);
+  window.open(`https://share.naver.com/web/shareView?url=${url}&title=${title}`);
+}
+
+const shareFacebook = () => {
+  const url = encodeURIComponent(window.location.href);
+  window.open(`http://www.facebook.com/sharer/sharer.php?u=${url}`);
 }
 
 const shareX = () => {
   const text = encodeURIComponent("나의 에임 점수는?");
   const url = encodeURIComponent(window.location.href);
-  window.open("https://twitter.com/intent/tweet?text=" + text + "&url=" + url);
+  window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}`);
 }
 
 btnLink.addEventListener('click', copyToClipBoard);
 btnNaver.addEventListener('click', shareNaver);
+btnFacebook.addEventListener('click', shareFacebook);
 btnX.addEventListener('click', shareX);
